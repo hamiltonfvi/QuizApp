b884cd23166e3de5a3fb3c68b1c20abd3725e8bc
Hamilton Jimenez <hamiltonjbeach7@gmail.com>
2018-06-12 16:15:53 -0400

    -added a reset button that clears everything -added sounds when the reset and results buttons are press -added a result button that brings the name of the participant along with the score and the results of the quiz.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.quizapp;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +9,9 @@ import android.widget.Toast;
 import android.widget.RadioButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String Final_Score = "savedScore";
     EditText quizPlayer;
     TextView questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix;
     TextView questionSeven, questionEight, questionNine, questionTen;
@@ -91,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void quizResults(View view) {
 
+        String name = quizPlayer.getText().toString();
+
         //One point for a corrected answer in question one
         if (responseOneA.isChecked()) {
             score = score + 1;
@@ -135,9 +136,86 @@ public class MainActivity extends AppCompatActivity {
 
         //Display the winner or the loser
         if (score >= 5) {
-            Toast.makeText(this, getString(R.string.winner), Toast.LENGTH_LONG).show();
+            String scoreMessage = getString(R.string.winner) + "\n" + name + "\n" + getString(R.string.score) + " " + score;
+            Toast.makeText(this, scoreMessage, Toast.LENGTH_LONG).show();
+            final MediaPlayer ring = MediaPlayer.create(this, R.raw.tada);
+            ring.start();
+
         } else {
-            Toast.makeText(this, getString(R.string.loser), Toast.LENGTH_LONG).show();
+            String scoreMessage = getString(R.string.loser) + "\n" + name + "\n" + getString(R.string.score) + " " + score;
+            Toast.makeText(this, scoreMessage , Toast.LENGTH_LONG).show();
+            final MediaPlayer ring = MediaPlayer.create(this, R.raw.lost);
+            ring.start();
         }
+
+        score = 0;
+    }
+
+    public void onReset(View view) {
+        if (responseOneA.isChecked())
+            responseOneA.toggle();
+        if (responseOneB.isChecked())
+            responseOneB.toggle();
+        if (responseOneC.isChecked())
+            responseOneC.toggle();
+        if (responseTwoA.isChecked())
+            responseTwoA.toggle();
+        if (responseTwoB.isChecked())
+            responseTwoB.toggle();
+        if (responseTwoC.isChecked())
+            responseTwoC.toggle();
+        if (responseThreeA.isChecked())
+            responseThreeA.setChecked(false);
+        if (responseThreeB.isChecked())
+            responseThreeB.setChecked(false);
+        if (responseThreeC.isChecked())
+            responseThreeC.setChecked(false);
+        if (responseFourA.isChecked())
+            responseFourA.toggle();
+        if (responseFourB.isChecked())
+            responseFourB.toggle();
+        if (responseFourC.isChecked())
+            responseFourC.toggle();
+        if (responseFiveA.isChecked())
+            responseFiveA.toggle();
+        if (responseFiveB.isChecked())
+            responseFiveB.toggle();
+        if (responseFiveC.isChecked())
+            responseFiveC.toggle();
+        if (responseSixA.isChecked())
+            responseSixA.toggle();
+        if (responseSixB.isChecked())
+            responseSixB.toggle();
+        if (responseSixC.isChecked())
+            responseSixC.toggle();
+        if (responseSevenA.isChecked())
+            responseSevenA.toggle();
+        if (responseSevenB.isChecked())
+            responseSevenB.toggle();
+        if (responseSevenC.isChecked())
+            responseSevenC.toggle();
+        if (responseEightA.isChecked())
+            responseEightA.toggle();
+        if (responseEightB.isChecked())
+            responseEightB.toggle();
+        if (responseEightC.isChecked())
+            responseEightC.toggle();
+        if (responseEightD.isChecked())
+            responseEightD.toggle();
+        if (responseEightE.isChecked())
+            responseEightE.toggle();
+        if (responseEightF.isChecked())
+            responseEightF.toggle();
+        if (responseNineA.isChecked())
+            responseNineA.toggle();
+        if (responseNineB.isChecked())
+            responseNineB.toggle();
+        if (responseNineC.isChecked())
+            responseNineC.toggle();
+        if (responseTen.isChecked())
+            responseTen.toggle();
+        recreate();
+        final MediaPlayer ring = MediaPlayer.create(this, R.raw.reset);
+        ring.start();
     }
 }
